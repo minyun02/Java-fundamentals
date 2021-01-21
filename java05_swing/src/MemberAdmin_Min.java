6bb011186fc26f8e38b9d2bf93b35e74f36b9005
minyun02 <www5981@naver.com>
2021-01-21 16:52:29 +0900

    0121

---
+++ java05_swing/src/MemberAdmin_Min.java
@@ -0,0 +1,21 @@
+import javax.swing.JFrame;
+
+public class MemberAdmin_Min extends JFrame {
+
+	public MemberAdmin_Min() {
+		super("회원관리");
+		
+		
+		
+		
+		
+		setSize(500,500);
+		setVisible(true);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+	}
+
+	public static void main(String[] args) {
+		new MemberAdmin_Min();
+	}
+
+}
